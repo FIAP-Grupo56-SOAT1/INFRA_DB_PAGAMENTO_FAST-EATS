1cd32863a297cd4c890e200bd0c0646d49d0916c
Jairo <hardtelles@gmail.com>
2024-02-18 20:57:17 -0300

    RDS pagamento

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "bucket-fiap-soat1-grupo56-remote-state"
-    key    = "aws-rds-fiap56-pagamento/terraform.tfstate"
+    bucket = "bucket-fiap56-to-remote-state"
+    key    = "aws-rds-pagamento-fiap56/terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -20,9 +20,8 @@ provider "aws" {
 }
 
 #create a security group for RDS Database Instance
-/*
-resource "aws_security_group" "rds_sg" {
-  name = "rds_sg"
+resource "aws_security_group" "rds_pagamento_sg" {
+  name = "rds_pagamento_sg"
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -36,24 +35,25 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-*/
 
 
-resource "aws_db_instance" "default" {
-  allocated_storage      = var.allocated_storage
-  storage_type           = var.storage_type
-  engine                 = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["engine"]
-  engine_version         = var.engine_version
-  instance_class         = var.instance_class
-  db_name                = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["dbname"]
-  username               = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["username"]
-  password               = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["password"]
-  port                   = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["port"]
-  identifier             = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["dbname"]
-  parameter_group_name   = var.parameter_group_name
-  skip_final_snapshot    = var.skip_final_snapshot
-  publicly_accessible    = true
-  #vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
+resource "aws_db_instance" "pagamento" {
+
+  allocated_storage       = var.allocated_storage
+  storage_type            = var.storage_type
+  engine                  = "mysql"
+  engine_version          = var.engine_version
+  instance_class          = var.instance_class
+  db_name                 = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["dbname"]
+  username                = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["username"]
+  password                = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["password"]
+  port                    = jsondecode(data.aws_secretsmanager_secret_version.mysql_credentials.secret_string)["port"]
+  identifier              = var.identifier
+  parameter_group_name    = var.parameter_group_name
+  skip_final_snapshot     = var.skip_final_snapshot
+  publicly_accessible     = true
+  vpc_security_group_ids  = [aws_security_group.rds_pagamento_sg.id]
+  backup_retention_period = 0
 }
 
 data "aws_secretsmanager_secret" "mysql" {
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 #outputs.tf
 
-/*
+
 output "security_group_id" {
-  value       = aws_security_group.rds_sg.id
+  value       = aws_security_group.rds_pagamento_sg.id
 }
-*/
+
 
 output "db_instance_endpoint" {
-  value       = aws_db_instance.default.endpoint
+  value       = aws_db_instance.pagamento.endpoint
 }
